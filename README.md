000aaf4996e51a33a2e2ada86eafdbc3566c0b66
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-05-10 18:34:09 +0200

    add readme + tiny small change to test function deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+# Super simple start to Firebase functions
+
+Source: [Super simple start to Firebase functions](https://kentcdodds.com/blog/super-simple-start-to-firebase-functions) by Kent C. Dodds
--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,6 @@ const functions = require("firebase-functions")
 module.exports = {
   hello: functions.https.onRequest((req, res) => {
     const subject = req.query.subject || "World"
-    res.send(`Hello ${subject}!`)
+    res.send(`Hello, ${subject}!`)
   }),
 }
